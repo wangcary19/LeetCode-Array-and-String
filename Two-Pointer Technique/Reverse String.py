c00d5424249fff6e390c7d43b25a222a78698e99
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2024-01-23 01:09:20 -0500

    Update Reverse String.py

--- Two-Pointer Technique/Reverse String.py
+++ Two-Pointer Technique/Reverse String.py
@@ -3,3 +3,6 @@
 def reverseString(self, s: List[str]) -> None:
     for i in range(0, int(len(s) / 2)):
         s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
+
+def reverseString_alt(self, s: List[str]) -> None:
+    s.reverse()
