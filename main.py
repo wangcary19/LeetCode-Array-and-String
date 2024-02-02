7a95f3b06308cb026d74d622761a022c6cee4bf4
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2024-02-02 15:12:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,18 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print(int(3/2))
+
+    min_len = len(nums)
+    for i in range(1, len(nums) - 1):  # list slice size
+        for j in range(0, len(nums) - i + 1):  # slice start
+            # print("Checking " + str(nums[j:j + i]) + ", i is " + str(i) + ", j is " + str(j))
+            if sum(nums[j:j + i]) >= target and i < min_len:
+                min_len = i
+                # print("min_len set to " + str(min_len))
+            else:
+                continue
+    # end-for
+    return min_len
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
