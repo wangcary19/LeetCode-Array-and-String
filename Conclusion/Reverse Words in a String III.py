7287ed97b55e2f60d35c0d189a7211a86bf16896
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2024-02-01 23:55:13 -0500

    Update Reverse Words in a String

--- Conclusion/Reverse Words in a String III.py
+++ Conclusion/Reverse Words in a String III.py
@@ -1,10 +1,4 @@
 # February 1st, 2024
 
 def reverseWords(self, s: str) -> str:
-    out = ""
-    for i in range(len(s) - 1, -1, -1):
-        out += s[i]
-    l = out.split()
-    l = reversed(l)
-    j = " ".join(l)
-    return j
+    return " ".join([x[::-1] for x in s.split()])
